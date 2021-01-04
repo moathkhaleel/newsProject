fe8639537b178e5e605ec7328bb946f00f2db401
Moath Khaleel <moathkhaleel@outlook.com>
2021-01-04 19:48:23 +0300

    initial

---
+++ news-project/src/client/js/formHandler.js
@@ -0,0 +1,59 @@
+import fetch from "node-fetch";
+
+function handleSubmit(event) {
+    event.preventDefault()
+
+    // check what text was put into the form field
+    let formText = document.getElementById('name').value.replace(/\s/g,"&");
+    console.log("fetch initiated");
+    Client.checkForName(formText)
+    fetch('http://localhost:8081/', {formText: formText})
+    .then(function(data) {
+        console.log(data.irony)
+        postData('/addText', data = {irony: data.irony, subjectivity: data.subjectivity})
+        updateUI()
+    })
+
+}
+
+const postData = async (url, data) => {
+    const response = await fetch(url, {
+       method: 'POST', 
+       credentials: 'same-origin', 
+       headers: {
+           'Content-Type': 'application/json',
+       },
+       body: JSON.stringify(data), 
+   });
+   try {
+       const newData = await response.json();
+       return newData
+   } catch(eror) {
+       console.log("error", error);
+   } 
+};
+
+const updateUI = async () => {
+    const request = await fetch('/all')
+    try {
+        const allData = await request.json()
+        console.log(allData);
+        document.getElementById('irony').innerHTML = allData.irony;
+        document.getElementById('subjectivity').innerHTML = allData.subjectivity;
+    } catch(error) {
+        console.log("error", error)
+    }
+}
+
+const makeCall = async(txt) => {
+    const res = await fetch(baseURL1 + apiKey + txt + baseURL2) 
+    try {
+        const data = await res.json();
+        console.log(data)
+        return data;
+    } catch(error) {
+        console.log("error", error);
+    } 
+}
+
+export { handleSubmit }
---
+++ news-project/src/server/index.js
@@ -0,0 +1,63 @@
+projectData = {};
+const baseURL1 = 'https://api.meaningcloud.com/sentiment-2.1?key=';
+const baseURL2 = '&lang=en';
+const apiKey = '10f35138a7823dddd2ea0d38211aa8bb&of=json&txt=';
+
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+dotenv.config();
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const cors = require('cors');
+const { default: fetch } = require('node-fetch');
+app.use(cors())
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    //res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!');
+})
+
+app.get('/all', getData)
+
+function getData(req,res) {
+    res.send(projectData)
+    console.log(projectData)
+    console.log('getdata complete')
+}
+
+app.post('/addText', addText);
+
+function addText(req,res) {
+    console.log('addText')
+    newEntry = {
+        irony: req.body.irony,
+        subjectivity: req.body.subjectivity
+    }
+    projectData = newEntry
+    res.send(projectData)
+    console.log(projectData)
+}
+
+
+app.get('/call', async (req, res) => {
+    const userInput = req.body; 
+    console.log(`user input is: ${userInput}`);
+    const apiURL = baseURL1 + apiKey + userInput + baseURL2;
+    const response = await fetch(apiURL);
+    const data = await response.json()
+    res.json(data);
+})
+
+
